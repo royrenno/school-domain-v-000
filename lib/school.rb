5b311101e95b2fda497fc19451ae2a5326796db4
roy renno <royrenno@yahoo.com>
2019-03-01 03:09:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,4 +11,19 @@ class School
     roster[grade]<< student_name
   end
 
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort 
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end 
+    sorted
+  end
+end
+
+    
+
   
