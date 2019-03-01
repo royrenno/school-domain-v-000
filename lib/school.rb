a28d1300737d97a66e569838c1f3182c82750750
roy renno <royrenno@yahoo.com>
2019-03-01 03:11:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,8 @@ class School
   end
 
   def add_student(student_name, grade)
-    roster[grade] || = []
-    roster[grade]<< student_name
+    roster[grade] ||= []
+    roster[grade] << student_name
   end
 
   def grade(student_grade)
