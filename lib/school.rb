d415256840094df2fe6de913edbd74ace9a36575
roy renno <royrenno@yahoo.com>
2019-03-01 03:03:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,6 +3,13 @@ class School
   att_accessor :name, :roster
   def initialize(name)
     @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] || = []
+    roster[grade]<< student_name
   end
 
   
+    
